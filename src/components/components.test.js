3a5992745f48842f6affb3d6460dd4353ba0971e
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-08-10 17:06:21 +0100

    test the functionality of the components and calculator and operate file

---
+++ src/components/components.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer  from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Home from './Home';
+import Calculator from './Calculator';
+import Navbar from './Navbar';
+import Quote from './Quote';
+
+it('renders correctly react-test-renderer', () => {
+  const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly react-test-renderer', () => {
+  const tree = renderer.create(<Quote />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly react-test-renderer', () => {
+  const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  <Router>
+    const tree = renderer.create(
+    <Navbar />
+    ).toJSON(); expect(tree).toMatchSnapshot();
+  </Router>;
+});
+
+
+
